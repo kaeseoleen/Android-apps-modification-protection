15da020269045d3079e60204181fd8eacfbd4deb
kaeseoleen <kaeseoleen@gmail.com>
2020-03-29 19:50:50 +0300

    Сырой UI

---
+++ ModificationSecurity/ModificationSecurity/AdsPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ModificationSecurity
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AdsPage : ContentPage
+	{
+		public AdsPage ()
+		{
+			InitializeComponent ();
+		}
+        private async void AdEndedButton_Click(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+	}
+}
---
+++ ModificationSecurity/ModificationSecurity/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ModificationSecurity
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void AddScoreButton_Click(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AdsPage());
+
+        }
+        private void ExitAppButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
